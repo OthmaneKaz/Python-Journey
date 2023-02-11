c8445555863cfc9549619d60835bc92afc501f98
Othmane Kazdar <88615382+OthmaneKaz@users.noreply.github.com>
2023-02-11 18:04:55 +0100

    update

--- Intermidiate Projects/Pong Game/main.py
+++ Intermidiate Projects/Pong Game/main.py
@@ -1,1 +1,23 @@
+from turtle import Screen
+from paddle import Paddle
 
+screen = Screen()
+screen.setup(800, 600)
+screen.bgcolor("black")
+screen.title("Pong Game")
+screen.tracer(0)
+
+r_paddle = Paddle((360, 0))
+l_paddle = Paddle((-360, 0))
+
+screen.listen()
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
+
+state = True
+while state:
+    screen.update()
+
+screen.exitonclick()
