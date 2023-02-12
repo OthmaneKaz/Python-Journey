6dcc84b939d7f9430580428f446a5a53879f6b0e
Othmane Kazdar <88615382+OthmaneKaz@users.noreply.github.com>
2023-02-12 11:09:54 +0100

    update

--- Intermidiate Projects/Turtle Crossing Game/main.py
+++ Intermidiate Projects/Turtle Crossing Game/main.py
@@ -1,2 +1,19 @@
+import time
 from turtle import Screen
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
 
+"""Create the main Screen"""
+screen = Screen()
+screen.setup(600, 600)
+screen.bgcolor('white')
+screen.title("Turtle Crossing Game")
+screen.tracer(0)
+
+state = True
+while state:
+    time.sleep(0.1)
+    screen.update()
+
+screen.exitonclick()
