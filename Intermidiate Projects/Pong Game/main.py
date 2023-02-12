0b1bda24436e9c87a9097aeada5416cb3a1d9197
Othmane Kazdar <88615382+OthmaneKaz@users.noreply.github.com>
2023-02-12 09:56:56 +0100

    update

--- Intermidiate Projects/Pong Game/main.py
+++ Intermidiate Projects/Pong Game/main.py
@@ -1,5 +1,7 @@
 from turtle import Screen
 from paddle import Paddle
+from ball import Ball
+import time
 
 screen = Screen()
 screen.setup(800, 600)
@@ -10,6 +12,8 @@ screen.tracer(0)
 r_paddle = Paddle((360, 0))
 l_paddle = Paddle((-360, 0))
 
+ball = Ball()
+
 screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
 screen.onkey(r_paddle.go_down, "Down")
@@ -19,5 +23,7 @@ screen.onkey(l_paddle.go_down, "s")
 state = True
 while state:
     screen.update()
+    ball.move()
+    time.sleep(0.1)
 
 screen.exitonclick()
