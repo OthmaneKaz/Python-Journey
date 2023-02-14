e108d73c7cf5a310dbcc841864014130f1f357a8
Othmane Kazdar <88615382+OthmaneKaz@users.noreply.github.com>
2023-02-14 12:42:48 +0100

    update

--- Intermidiate Projects/NATO Alphabet/main.py
+++ Intermidiate Projects/NATO Alphabet/main.py
@@ -1,1 +1,7 @@
+import pandas as pd
 
+data = pd.read_csv("nato_phonetic_alphabet.csv")
+phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
+
+word = input("Enter your word : ").upper()
+print([phonetic_dict[letter] for letter in word])
